350e987be8244580de4c7492cfdc1c01409ca65b
Alexander Kolesnikov <akolesnikov@l0241046.ista.local>
2014-05-17 19:20:43 +0200

    speedup

--- pyutils/utils.py
+++ pyutils/utils.py
@@ -3,8 +3,14 @@ import numpy as np
 
 import collections
 
+import functools
 
-def save_dict_hdf5(d, file_name, compress=5, predictive=True, types={}):
+
+def compose(*functions):
+    return functools.reduce(lambda f, g: lambda x: f(g(x)), functions[::-1])
+
+
+def save_dict_hdf5(d, file_name, compress=5, types={}):
     filters = tables.Filters(complib='zlib', complevel=compress)
     with tables.open_file(file_name, mode='w', filters=filters) as hdf5:
         for name, array in d.items():
@@ -12,29 +18,28 @@ def save_dict_hdf5(d, file_name, compress=5, predictive=True, types={}):
                 converter = eval('%s_to_array' % types[name])
                 array = converter(array)
             assert array.dtype.kind in ['i', 'f'], 'Only signed integers are supported'
-            hdf5.create_carray('/', name, obj=encode_diff(array) if predictive else array)
-        type_table_format = {'key': tables.StringCol(itemsize=30), 'serialize': tables.StringCol(itemsize=30)}
+            hdf5.create_carray('/', name, obj=encode_diff(array))
+        type_table_format = {'key': tables.StringCol(itemsize=50), 'serialize': tables.StringCol(itemsize=30)}
 
         hdf5.createTable('/', '__types__', type_table_format)
         if types:
             hdf5.root.__types__.append(types.items())
 
 
-def load_dict_hdf5(file_name, predictive=True):
-    with tables.open_file(file_name, mode='r') as hdf5:
-        types = dict(hdf5.root.__types__.read())
-        root_node = hdf5.get_node('/')
-        fields = filter(lambda s: not s.startswith("_") and not s == '__types__', dir(root_node))
-        d = dict([(field, np.array(getattr(root_node, field))) for field in fields])
-    if predictive:
-        for name, array in d.items():
-            array = decode_diff(array)
-            if name in types:
-                converter = eval('array_to_%s' % types[name])
-                d[name] = converter(array)
-            else:
-                d[name] = array
-    return d
+def load_dict_hdf5(file_name):
+    hdf5 = tables.open_file(file_name, mode='r')
+
+    types = dict(hdf5.root.__types__.read())
+    fields = filter(lambda s: not s.startswith("_") and not s == '__types__', dir(hdf5.root))
+    d = dict([(field, [functools.partial(lambda f, dummy: np.array(f), getattr(hdf5.root, field)), decode_diff]) for field in fields])
+    for name, array in d.items():
+        if name in types:
+            d[name] += [eval('array_to_%s' % types[name])]
+
+    for name, functors in d.items():
+        d[name] = compose(*functors)
+
+    return hdf5, d
 
 
 def encode_diff(a):
